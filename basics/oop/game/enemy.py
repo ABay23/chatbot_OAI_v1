0fe80973860ab511d14adaceca9776c0a10a354a
Alejandro Bay <alejandrobay23@gmail.com>
2025-05-08 16:09:52 -0400

    New oop interaction

---
+++ basics/oop/game/enemy.py
@@ -0,0 +1,10 @@
+'''This is the Enemy Class with the Enemy State and methods'''
+
+class Enemy_alien:
+    name: str = 'Rarg'
+    health_points:int = 10
+    eyes: str = 3
+    attack_speed: str = 'slow'
+    attack_damage: int = 1
+    
+    
