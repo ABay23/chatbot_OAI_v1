1faaa8c19d14cf41bd1cf99b1f3269f58364a0c6
Alejandro Bay <alejandrobay23@gmail.com>
2025-05-08 11:37:44 -0400

    Multiple updates on test

--- basics_1.py
+++ basics_1.py
@@ -9,18 +9,44 @@ Testing mat:
 - Return total printed.
 '''
 
-bank = 50
-item = 15
-tax = 3 / 100
+# bank = 50
+# item = 15
+# tax = 3 / 100
 
-def purchasing(money: int, price: int, tax: float)-> float:
-    total = (price + (price * tax))
-    money = money - total
-    print(f'Your total after purchasin is {total}, you have {money} in your account.')
+# def purchasing(money: int, price: int, tax: float)-> float:
+#     total = (price + (price * tax))
+#     money = money - total
+#     print(f'Your total after purchasin is {total}, you have {money} in your account.')
     
-purchasing(bank, item, tax)
+# purchasing(bank, item, tax)
 
-'''This is a format string method'''
-name = 'Alejandro'
-sentence = 'Hi {}'
-print(sentence.format(name))
+# '''This is a format string method'''
+# name = 'Alejandro'
+# lastname = 'Doe'
+# sentence = 'Hi {}'
+# print(sentence.format(name, lastname))
+
+# '''Pla,ying with sets'''
+
+# my_set = {3, 4, 5, 5, 3, 7, 9}
+# my_set.discard(3)
+
+# my_set.update([16, 2])
+
+# my_set.add('one')
+
+# print(my_set)
+
+'''Loops and while loop test on continue'''
+i = 0
+while i < 6:
+    # print(i)
+    i += 1
+    
+    if i == 3:
+        continue
+    print(i)
+    # if i == 4:
+        # break
+else:
+    print('i is now larger or equal to 6')
