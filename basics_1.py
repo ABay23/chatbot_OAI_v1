aec2c4f2c73d02e22f2c0a197761952d59a4117b
Alejandro Bay <alejandrobay23@gmail.com>
2025-05-08 10:48:45 -0400

    first commit for testing purposes

---
+++ basics_1.py
@@ -0,0 +1,21 @@
+# 
+'''
+This is a special comment updated on the IDE
+
+Testing mat:
+- You have $50
+- You buy an item $15
+- Add 3% tax
+- Return total printed.
+'''
+
+bank = 50
+item = 15
+tax = 3 / 100
+
+def purchasing(money: int, price: int, tax: float)-> float:
+    total = (price + (price * tax))
+    money = money - total
+    print(f'Your total after purchasin is {total}, you have {money} in your account.')
+    
+purchasing(bank, item, tax)
