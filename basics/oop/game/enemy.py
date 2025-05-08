9a9c846430c9a63cd09e38c5cbe5be5a07975845
Alejandro Bay <alejandrobay23@gmail.com>
2025-05-08 19:00:24 -0400

    Added Getters and setters for constructor

--- basics/oop/game/enemy.py
+++ basics/oop/game/enemy.py
@@ -1,12 +1,31 @@
 '''This is the Enemy Class with the Enemy State and methods'''
 
 class Enemy_alien:
-    def __init__(self):
+    def __init__(self, the_name: str, health_points: int = 10, eyes :int =2, 
+                attack_speed : str ='medium',attack_damage : int = 1,
+                type_of_enemy :str = 'Lizard'
+                ):
+        self.the_name = the_name
+        self.health_points = health_points
+        self.eyes = eyes
+        self.attack_speed = attack_speed
+        self.attack_damage = attack_damage
+        self.__type_of_enemy = type_of_enemy
+        
+    def get_type_of_enemy(self):
+        return self.__type_of_enemy
     
-        name: str = 'Rarg'
-        health_points:int = 10
-        eyes: str = 3
-        attack_speed: str = 'slow'
-        attack_damage: int = 1
+    # def set_type_of_enemy(self, type_of_enemy):
+    #     self.__type_of_enemy = type_of_enemy
+    
+    def enemy_talk(self):
+        print('Woof Woof')
+        
+    def attack(self):
+        print(f'Enemy {self.__type_of_enemy} attack with {self.attack_damage}')
+        
+    def walk_forward(self):
+        print(f'Enemy {self.__type_of_enemy} walks closer to you')
+        
     
     
--- basics/oop/game/main.py
+++ basics/oop/game/main.py
@@ -1,11 +1,20 @@
 '''We are executing on main using imported classes'''
 from enemy import *
 
-enemy = Enemy_alien()
+lizard = Enemy_alien(the_name= 'Rarg', health_points = 10, eyes = 3, attack_speed = 'slow', 
+                    attack_damage = 2, type_of_enemy='monkey')
 
 '''we can add another feature and make it part of the class'''
-enemy.type_of_enemy= 'Lizard'
+# enemy.type_of_enemy= 'Lizard'
+enemy_type = lizard.get_type_of_enemy()
+lizard.enemy_talk()
+lizard.walk_forward()
+lizard.attack()
 
-print(f'My name is {Enemy_alien.name}, I have {Enemy_alien.eyes} eyes and' + 
-    f"I'm a {Enemy_alien.attack_speed} {enemy.type_of_enemy}!")
+
+
+print(f'My name is {lizard.the_name}, I have {lizard.eyes} eyes and' + 
+    f"I'm a {lizard.attack_speed} {enemy_type}!")
+
+print(lizard)
 
