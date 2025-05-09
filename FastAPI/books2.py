0fb439929b593759ce2b75d10f9c1decd5275657
Alejandro Bay <alejandrobay23@gmail.com>
2025-05-08 23:38:15 -0400

    Added mock data books and changed config endpoint

--- FastAPI/books2.py
+++ FastAPI/books2.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from mock_data import BOOKS
 
 app = FastAPI()
 
-@app.get('/')
-async def first_api():
-    return {'message': 'We did it!!!'}
+@app.get('/books')
+async def read_all_books():
+    return BOOKS
---
+++ FastAPI/mock_data.py
@@ -0,0 +1,22 @@
+'''Mock Data for testing purposes'''
+from typing import List, Dict
+
+BOOKS: List[Dict[str, str]] = [
+    {'title': 'The Hobbit', 'author': 'J.R.R. Tolkien', 'category': 'Fantasy'},
+    {'title': 'The Fellowship of the Ring', 'author': 'J.R.R. Tolkien', 'category': 'Fantasy'},
+    {'title': 'The Two Towers', 'author': 'J.R.R. Tolkien', 'category': 'Fantasy'},
+    {'title': 'The Return of the King', 'author': 'J.R.R. Tolkien', 'category': 'Fantasy'},
+    {'title': 'The Silmarillion', 'author': 'J.R.R. Tolkien', 'category': 'Mythology'},
+    
+    {'title': "Harry Potter and the Sorcerer's Stone", 'author': 'J.K. Rowling', 'category': 'Fantasy'},
+    {'title': 'Harry Potter and the Chamber of Secrets', 'author': 'J.K. Rowling', 'category': 'Fantasy'},
+    {'title': 'Harry Potter and the Prisoner of Azkaban', 'author': 'J.K. Rowling', 'category': 'Fantasy'},
+    {'title': 'Harry Potter and the Goblet of Fire', 'author': 'J.K. Rowling', 'category': 'Fantasy'},
+    {'title': 'Harry Potter and the Order of the Phoenix', 'author': 'J.K. Rowling', 'category': 'Fantasy'},
+    
+    {'title': 'A Game of Thrones', 'author': 'George R.R. Martin', 'category': 'Fantasy'},
+    {'title': 'A Clash of Kings', 'author': 'George R.R. Martin', 'category': 'Fantasy'},
+    {'title': 'A Storm of Swords', 'author': 'George R.R. Martin', 'category': 'Fantasy'},
+    {'title': 'A Feast for Crows', 'author': 'George R.R. Martin', 'category': 'Fantasy'},
+    {'title': 'A Dance with Dragons', 'author': 'George R.R. Martin', 'category': 'Fantasy'}
+]
