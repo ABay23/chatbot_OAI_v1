643332ca224f4bc7d5830717067f874471600635
Alejandro Bay <alejandrobay23@gmail.com>
2025-05-09 08:28:47 -0400

    Added new endpoint

--- FastAPI/books2.py
+++ FastAPI/books2.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get('/books')
 async def read_all_books():
     return BOOKS
+
+
+@app.get("/books/{dynamic_param}")
+async def read_all_books(dynamic_param):
+    return {'dynamic_param': dynamic_param}
