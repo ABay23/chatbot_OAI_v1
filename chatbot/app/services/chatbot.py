d7f5ea5b7b20246b56ed8ab141f2032cbf929456
Alejandro Bay <alejandrobay23@gmail.com>
2025-05-09 19:33:27 -0400

    Add chat log support to preserve previous messages for conversational context

--- chatbot/app/services/chatbot.py
+++ chatbot/app/services/chatbot.py
@@ -8,18 +8,22 @@ api_key = os.getenv('OPEN_AI_KEY')
 
 client =  OpenAI(api_key= api_key)
 
+chat_log = []
 
+while True:
+    user_input = input("How may I help you today? or type 'quit' to leave chat.")
+    
+    if user_input.casefold() == 'quit':
+        break
+    
+    chat_log.append({'role': 'user', 'content': user_input})
 
-response = client.chat.completions.create(
-    model='gpt-4o-mini',
-    messages=[{
-        'role': 'system',
-        'content': 'You are a helpful assistant'
-    },{
-        'role': 'user',
-        'content': 'Write me a 4 lines bio'
-    }],
-    temperature=0.8
-)
-
-print(response.choices[0].message.content)
+    response = client.chat.completions.create(
+        model='gpt-4o-mini',
+        messages=chat_log,
+        temperature=0.8
+    )
+    bot_response = response.choices[0].message.content
+    chat_log.append({'role': 'assistant', 'content': bot_response})
+    print(bot_response)
+    
