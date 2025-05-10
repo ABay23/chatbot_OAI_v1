c68eb0adfd2a4aeb3ca00f6e70a07b8828277092
Alejandro Bay <alejandrobay23@gmail.com>
2025-05-09 21:15:19 -0400

    Changed route from server and HTML template

--- chatbot/app/main.py
+++ chatbot/app/main.py
@@ -1,8 +1,17 @@
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 from fastapi import FastAPI, Form
 from typing import Annotated
 from services.chatbot import get_chatbot_response
+from fastapi import Request
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory='app/templates')
+
+@app.get('/', response_class=HTMLResponse)
+async def chat_page(request: Request):
+    return templates.TemplateResponse("layout.html",{"request": request})
 
 @app.post('/')
 async def chat(user_input: Annotated[str, Form()]):
