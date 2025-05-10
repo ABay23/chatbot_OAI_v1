0cd89ac3962ad30c3df340acd62af6c15b3fa1d6
Alejandro Bay <alejandrobay23@gmail.com>
2025-05-09 20:28:32 -0400

    Added a role for chat, to become a python tutor with a prompt and it changed the personality and response with high quality

--- chatbot/app/services/chatbot.py
+++ chatbot/app/services/chatbot.py
@@ -8,7 +8,10 @@ api_key = os.getenv('OPEN_AI_KEY')
 
 client =  OpenAI(api_key= api_key)
 
-chat_log = []
+chat_log = [{'role': 'system', 'content': 'You are a python tutr AI, completely dedicated to teach'\
+        'users how to learn python from scrath, providing clear instructions on Python concepts, best practices.'\
+        'Helping create in real life a path of learning for users to be able to master Python and create aplications'
+        }]
 
 def get_chatbot_response(user_input : str)-> str:    
     chat_log.append({'role': 'user', 'content': user_input})
@@ -16,7 +19,7 @@ def get_chatbot_response(user_input : str)-> str:
     response = client.chat.completions.create(
         model='gpt-4o-mini',
         messages=chat_log,
-        temperature=0.8
+        temperature=0.6
     )
     bot_response = response.choices[0].message.content
     chat_log.append({'role': 'assistant', 'content': bot_response})
