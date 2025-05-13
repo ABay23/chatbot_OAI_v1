5bca551725d33f8fc18d3f2b4e15217457d06782
Alejandro Bay <alejandrobay23@gmail.com>
2025-05-12 21:55:17 -0400

    THe new endpoint is set with a decorator

--- FastAPI/books3.py
+++ FastAPI/books3.py
@@ -1,1 +1,9 @@
 '''Books ans CRUD '''
+from fastapi import FastAPI
+
+app = FastAPI()
+
+'''First Get MethodReques and API endpoint'''
+@app.get('/first_endpoint')
+async def first_api():
+    print({'Name': 'Alejandro'})
