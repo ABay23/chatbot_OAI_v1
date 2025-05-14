53556aaa836b904d44439d86bf906a74222b18d5
Alejandro Bay <alejandrobay23@gmail.com>
2025-05-13 21:18:51 -0400

    Configured get mixing Dynamic Params and Query Params

--- FastAPI/books3.py
+++ FastAPI/books3.py
@@ -28,3 +28,18 @@ async def get_books_by_category(category: str):
         return book_category
     except ValueError as e:
         raise HTTPException(status_code=404, detail= f'Category not found : {str(e)}')
+    
+'''Get mixing dynamic Params and Query params'''
+
+@app.get('/books/{book_author}/')
+async def get_books_author_and_Category(book_author: str, book_category: str):
+    books_return = []
+    # try:
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and \
+            book.get('category').casefold() == book_category.casefold():
+                books_return.append(book)
+                
+    return books_return
+    # except ValueError as e:
+    #     raise HTTPException(status_code=404, detail= f'No match found: {str(e)}')
