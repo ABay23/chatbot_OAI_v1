e6b3609853513267dc4050ab09900737c25cde06
Alejandro Bay <alejandrobay23@gmail.com>
2025-05-13 20:57:46 -0400

    The return was shoing an error on the query parameters on the GET request- fixed

--- FastAPI/books3.py
+++ FastAPI/books3.py
@@ -1,5 +1,5 @@
 '''Books ans CRUD '''
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from mock_data import BOOKS
 
 app = FastAPI()
@@ -15,3 +15,16 @@ async def get_book(book_title : str):
     for book in BOOKS:
         if book.get('title').casefold() == book_title.casefold():
             return book
+        
+'''Get books by Category'''
+
+@app.get('/books/')
+async def get_books_by_category(category: str):
+    book_category = []
+    try:
+        for book in BOOKS:
+            if book.get('category').casefold() == category.casefold():
+                book_category.append(book)
+        return book_category
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail= f'Category not found : {str(e)}')
