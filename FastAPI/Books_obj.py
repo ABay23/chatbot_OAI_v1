9218bed626127088adbd148446e6f1a36d40fdee
Alejandro Bay <alejandrobay23@gmail.com>
2025-05-14 15:48:17 -0400

    New post request created and error handler in place

--- FastAPI/Books_obj.py
+++ FastAPI/Books_obj.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
-from mock_data import BOOKS
+from fastapi import FastAPI, Body, HTTPException
+from mock_data import BOOKS, Book
 
 app = FastAPI()
 
-@app.get('/')
+@app.get('/books')
 async def get_all_books():
     return BOOKS
+
+@app.post('/create-book')
+async def create_book(book_request = Body()):
+    try:
+        BOOKS.append(book_request)
+        return {'message': 'New book Created!'}
+    except AttributeError as e:
+        raise HTTPException(status_code=400, detail= f'Bad Request {str(e)}')
