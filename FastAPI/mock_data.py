f82bb432f9979fcf02aa1e7a7acc0ade35205202
Alejandro Bay <alejandrobay23@gmail.com>
2025-05-14 15:14:20 -0400

    NEw FastAPI setting with objects and return

--- FastAPI/mock_data.py
+++ FastAPI/mock_data.py
@@ -1,25 +1,25 @@
 """Mock Data for testing purposes"""
 from typing import List, Dict
-
-# BOOKS: List[Dict[str, str]] = [
-#     {"title": "The Hobbit", "author": "J.R.R. Tolkien", "category": "Fantasy"},
-#     {"title": "The Fellowship of the Ring", "author": "J.R.R. Tolkien", "category": "Fantasy"},
-#     {"title": "The Two Towers", "author": "J.R.R. Tolkien", "category": "Fantasy"},
-#     {"title": "The Return of the King", "author": "J.R.R. Tolkien", "category": "Fantasy"},
-#     {"title": "The Silmarillion", "author": "J.R.R. Tolkien", "category": "Mythology"},
+'''
+BOOKS: List[Dict[str, str]] = [
+    {"title": "The Hobbit", "author": "J.R.R. Tolkien", "category": "Fantasy"},
+    {"title": "The Fellowship of the Ring", "author": "J.R.R. Tolkien", "category": "Fantasy"},
+    {"title": "The Two Towers", "author": "J.R.R. Tolkien", "category": "Fantasy"},
+    {"title": "The Return of the King", "author": "J.R.R. Tolkien", "category": "Fantasy"},
+    {"title": "The Silmarillion", "author": "J.R.R. Tolkien", "category": "Mythology"},
     
-#     {"title": "Harry Potter and the Sorcerer"s Stone", "author": "J.K. Rowling", "category": "Fantasy"},
-#     {"title": "Harry Potter and the Chamber of Secrets", "author": "J.K. Rowling", "category": "Fantasy"},
-#     {"title": "Harry Potter and the Prisoner of Azkaban", "author": "J.K. Rowling", "category": "Fantasy"},
-#     {"title": "Harry Potter and the Goblet of Fire", "author": "J.K. Rowling", "category": "Fantasy"},
-#     {"title": "Harry Potter and the Order of the Phoenix", "author": "J.K. Rowling", "category": "Fantasy"},
+    {"title": "Harry Potter and the Sorcerer"s Stone", "author": "J.K. Rowling", "category": "Fantasy"},
+    {"title": "Harry Potter and the Chamber of Secrets", "author": "J.K. Rowling", "category": "Fantasy"},
+    {"title": "Harry Potter and the Prisoner of Azkaban", "author": "J.K. Rowling", "category": "Fantasy"},
+    {"title": "Harry Potter and the Goblet of Fire", "author": "J.K. Rowling", "category": "Fantasy"},
+    {"title": "Harry Potter and the Order of the Phoenix", "author": "J.K. Rowling", "category": "Fantasy"},
     
-#     {"title": "A Game of Thrones", "author": "George R.R. Martin", "category": "Fantasy"},
-#     {"title": "A Clash of Kings", "author": "George R.R. Martin", "category": "Fantasy"},
-#     {"title": "A Storm of Swords", "author": "George R.R. Martin", "category": "Fantasy"},
-#     {"title": "A Feast for Crows", "author": "George R.R. Martin", "category": "Fantasy"},
-#     {"title": "A Dance with Dragons", "author": "George R.R. Martin", "category": "Fantasy"}
-# ]
+    {"title": "A Game of Thrones", "author": "George R.R. Martin", "category": "Fantasy"},
+    {"title": "A Clash of Kings", "author": "George R.R. Martin", "category": "Fantasy"},
+    {"title": "A Storm of Swords", "author": "George R.R. Martin", "category": "Fantasy"},
+    {"title": "A Feast for Crows", "author": "George R.R. Martin", "category": "Fantasy"},
+    {"title": "A Dance with Dragons", "author": "George R.R. Martin", "category": "Fantasy"}
+]
 
 BOOKS: list[dict[str, str]] = [
     {"title": "The Hobbit", "author": "J.R.R. Tolkien", "category": "Fantasy"},
@@ -67,3 +67,18 @@ def categories(books):
     return categories
 
 print(categories(BOOKS))
+'''
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+    
+    
+    def __init__(self, id:int, title: str, author: str, description: str, rating: int):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
