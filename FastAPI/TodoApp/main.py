5f4c5fabf9027820338a2ed7c492a04737944d09
Alejandro Bay <alejandrobay23@gmail.com>
2025-05-18 13:17:59 -0400

    Added valitation methods for endpoints create Todo and Update Todo using id validation

--- FastAPI/TodoApp/main.py
+++ FastAPI/TodoApp/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Depends, HTTPException
+import stat
+from fastapi import FastAPI, Depends, HTTPException, Path
+from pydantic import BaseModel, Field
 import models
 from models import Todos
 from database import engine, SessionLocal
@@ -18,6 +20,13 @@ def get_db():
         db.close()
         
 db_dependency = Annotated[Session, Depends(get_db)]
+
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description : str = Field(max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete : bool
+
         
 '''THis is the get all query first test'''
 @app.get('/')
@@ -26,8 +35,36 @@ async def read_all(db: db_dependency):
 
 '''Get todo by ID'''
 @app.get('/todo/{todo_id}', status_code=status.HTTP_200_OK)
-async def read_todo(db: db_dependency, todo_id : int):
+async def read_todo(db: db_dependency, todo_id : int = Path(gt=0)):
     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404, detail='Todo Not found')
+
+'''POST on the new DB setting'''
+@app.post('/todo', status_code=status.HTTP_201_CREATED)
+async def create_todo(
+    db: db_dependency, 
+    todo_request: TodoRequest
+):
+    new_todo = Todos(**todo_request.model_dump())
+    
+    db.add(new_todo)
+    db.commit()
+    return {'message': f'New Todo Created!!!'}
+
+'''PUT Update a todo'''
+@app.put('/todo/{todo_id}')
+async def update_todo(db : db_dependency, todo_id : int, todo_request : TodoRequest ):
+    todo_update = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_update is None:
+        raise HTTPException(status_code=404, detail='Todo not found')
+    
+    todo_update.title = todo_request.title
+    todo_update.description = todo_request.description
+    todo_update.priority = todo_request.priority
+    todo_update.complete = todo_request.complete
+    
+    db.add(todo_update)
+    db.commit()
+    
