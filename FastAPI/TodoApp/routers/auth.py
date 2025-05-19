086ef0c476a0a458e17920908fa6625fe5d5a7e8
Alejandro Bay <alejandrobay23@gmail.com>
2025-05-19 17:52:25 -0400

    Updated DB

--- FastAPI/TodoApp/routers/auth.py
+++ FastAPI/TodoApp/routers/auth.py
@@ -1,9 +1,18 @@
-from fastapi import FastAPI, APIRouter
+from typing import Annotated
+from fastapi import Depends, FastAPI, APIRouter
 from pydantic import BaseModel
+from database import SessionLocal
 from models import Users
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
+
 
 # app = FastAPI()  #* We don't use FastAPI for endpoints, auth endpoints are created usng router
 router = APIRouter()
+
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 class CreateUserRequest(BaseModel):
     username: str
@@ -12,18 +21,39 @@ class CreateUserRequest(BaseModel):
     last_name: str
     password: str
     role: str
+    
+    
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+db_dependency = Annotated[Session, Depends(get_db)]
 
 '''Create User'''
-@router.post('/auth/')
-async def create_user(create_user_request : CreateUserRequest):
+@router.post('/auth', status_code=status.HTTP_201_CREATED)
+async def create_user(
+    db : db_dependency,
+    create_user_request : CreateUserRequest):
     create_user_model = Users(
         email=create_user_request.email,
         username=create_user_request.username,
         first_name=create_user_request.first_name,
         last_name=create_user_request.last_name,
-        hashed_password=create_user_request.password,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
         is_active = True
         
     )
     
-    return create_user_model
+    db.add(create_user_model)
+    db.commit()
+    return {'message': f'New user ({create_user_model.username}) created'}
+
+'''POST Request'''
+@router.post('/token')
+async def login_for_access_token(
+    form_data : Annotated[OAuth2PasswordRequestForm, Depends()],
+    db : db_dependency):
+    return 'token-up'
