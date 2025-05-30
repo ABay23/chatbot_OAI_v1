828354a7013f04b61026013ae83aefa11942926a
Alejandro Bay <alejandrobay23@gmail.com>
2025-05-30 00:00:32 -0400

    New db

--- FastAPI/TodoApp/database.py
+++ FastAPI/TodoApp/database.py
@@ -2,9 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
+POSTGRES_DB_URL = 'ostgresql://testing_1/Test1234!@localhost/TodoApplicationDatabase'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread' : False})
+# engine = create_engine(SQLALCHEMY_DATABASE_URLPO, connect_args={'check_same_thread' : False})
+engine = create_engine(POSTGRES_DB_URL)
 
 SessionLocal = sessionmaker(autocommit = False, autoflush= False, bind= engine)
 
