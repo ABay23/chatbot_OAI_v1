58516fd68ab3fae7a5cd71eeeeb383681e55b41b
Alejandro Bay <alejandrobay23@gmail.com>
2025-05-30 00:29:43 -0400

    THe DB was updated to postgreSQL and configured

--- FastAPI/TodoApp/database.py
+++ FastAPI/TodoApp/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
-POSTGRES_DB_URL = 'ostgresql://testing_1/Test1234!@localhost/TodoApplicationDatabase'
+POSTGRES_DB_URL = 'postgresql://postgres:Test1234!@localhost/TodoApplicationDatabase'
 
 # engine = create_engine(SQLALCHEMY_DATABASE_URLPO, connect_args={'check_same_thread' : False})
 engine = create_engine(POSTGRES_DB_URL)
